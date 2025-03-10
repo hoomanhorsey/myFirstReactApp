fa6d146713e50ffaa26e4b843dd53d6318d1621b
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2025-03-11 08:56:27 +1100

    Set up react app

---
+++ src/Functions.jsx
@@ -0,0 +1,9 @@
+function Greeting() {
+  return <h1>&quot; Hello there. I think this is the new beginning.&quot; </h1>;
+}
+
+function TrashTalk() {
+  return <h2>You're a terrible thingy you are</h2>;
+}
+
+export { TrashTalk, Greeting };
